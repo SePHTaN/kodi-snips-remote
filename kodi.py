159db8b26710b3e7a704f2ded4776df54d548be4
SePHTaN <sysiphus@t-online.de>
2019-11-01 13:51:28 +0100

    alternatve methode für den partymodus

--- kodi.py
+++ kodi.py
@@ -311,9 +311,11 @@ def partymode():
     ausgabe('partymode',1)
     #json_data = get_active_player()
     #if json_data != [] and json_data:
-    data_method= '"method":"Player.SetPartymode"'
+    #data_method= '"method":"Player.SetPartymode"'
+    data_method= '"method":"Player.Open"'
     #data_prop = ',"params":{"playerid":'+str(json_data['playerid'])+',"partymode":true}'
-    data_prop = ',"params":{"playerid":1,"partymode":true}'
+    #data_prop = ',"params":{"playerid":1,"partymode":true}'
+    data_prop = ',"params":{"item":{"partymode":"music"}}'
     data = data_method + data_prop
     send(data,1,'','partymode')
     return
